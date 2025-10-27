4377cfd0eb97abcb5d148a92aa5d12aaa26ed9ca
Nathaly <nathy.zulmira@gmail.com>
2025-10-27 11:06:59 -0300

    add função soma estoque

--- pages/estoque.php
+++ pages/estoque.php
@@ -23,6 +23,34 @@ function validarProduto($produtos){
     }
     return true; 
 }
+function somaEstoque($quantidades, $produtos){
+    $totais = [];
+    for ($i = 0; $i < count($produtos); $i++) {
+        $produtoMinusculo = mb_strtolower($produtos[$i]);
+        $soma = 0;
+
+        if (is_array($quantidades[$i])) {
+            foreach ($quantidades[$i] as $quantidade) {
+                $soma += $quantidade;
+            }
+        } else {
+            if ($quantidades[$i]) {
+                $soma = $quantidades[$i];
+            }
+        }
+        if (!isset($totais[$produtoMinusculo])) {
+            $totais[$produtoMinusculo] = ['nome_original' => $produtos[$i], 'totalQuant' => 0];
+        }
+        $totais[$produtoMinusculo]['totalQuant'] += $soma;
+    }
+    foreach ($totais as $info) {
+        $produto = $info['nome_original'];
+        echo "<p>Produto: ".$produto."<p/>".
+                "<p>Quantidade em estoque: ". $info['totalQuant']."<p/>";
+    }
+    return $totais;
+}
+
 $estoque = [
     'produto' => $_GET["produto"],
     'filial' => $_GET["filial"],
@@ -30,7 +58,8 @@ $estoque = [
 ];
 if(validarEntradas($estoque)==true){
     if(validarQuantidade($estoque['quantidade']) == true && validarProduto($estoque['produto'])==true){
-        echo "campos certos";
+        echo somaEstoque($estoque['quantidade'], $estoque['produto']);
+        
     }else{
         header("Location:../index.html");
     }
@@ -38,4 +67,7 @@ if(validarEntradas($estoque)==true){
 else{
     header("Location:../index.html");
 }
+
+
+
 ?>
